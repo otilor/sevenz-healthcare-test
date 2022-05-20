5625383b13d2e145e00bb9611a34ecf7e1f57279
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-05-20 12:02:43 +0100

    Rename table

---
+++ database/migrations/2022_05_20_105922_rename_subcategories_table_to_sub_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('subcategories', 'sub_categories');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sub_categories', function (Blueprint $table) {
+            //
+        });
+    }
+};
