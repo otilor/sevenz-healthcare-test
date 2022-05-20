5efb21a90f33e28d08bc2ed6fd3a0ee6516c515b
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-05-20 11:58:33 +0100

    Call seeder within database seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategorySeeder::class);
+        $this->call(SubCategorySeeder::class);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
